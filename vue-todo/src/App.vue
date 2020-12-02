76a81764ee62a69f0cfcbb3ff54c33d23d527173
DDOE49 <yhwy1965@naver.com>
2020-12-02 17:47:05 +0900

    할 일 관리 웹

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput  v-on:addTodoItem="addOneItem"></TodoInput>
+    <TodoList   v-bind:propsdata="todoItems" v-on:removeTodo="removeTodo" v-on:toggleItem="toggleOneItem"></TodoList>
+    <TodoFooter v-on:removeAll="clearAllItems"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  data() {
+    return {
+      todoItems: []
+    }
+  },
+  created() {
+    if (localStorage.length > 0)
+    {
+      for (var i = 0; i < localStorage.length; i++)
+      {
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
+        {
+          // this.todoItems.push(localStorage.key(i));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+  },
+  methods: {
+    addOneItem(todoItem) {
+      var obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+    clearAllItems() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+    toggleOneItem(todoItem, index) {
+      // todoItem.completed = !todoItem.completed;
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      // 로컬스토리지에 데이터 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }
+  },
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+  body {
+    text-align: center;
+    background-color: #F6F6F8;
+  }
+  input {
+    border-style: groove;
+    width: 200px;
+  }
+  button {
+    border-style: groove;
+  }
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  }
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,84 @@
+<template lang="html">
+  <section>
+    <div>
+        <ul>
+            <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+                <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted:todoItem.completed}" @click="toggleComplete(todoItem, index)" aria-hidden="true"></i>
+                <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+                <span class="removeBtn" type="button" v-on:click="removeTodo(todoItem, index)">
+                <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['propsdata'],
+  // data() {
+  //   return {
+  //     todoItems: []
+  //   }
+  // },
+  // created() { //App.vue로 이동
+  //   if (localStorage.length > 0)
+  //   {
+  //     for (var i = 0; i < localStorage.length; i++)
+  //     {
+  //       this.todoItems.push(localStorage.key(i));
+  //     }
+  //   }
+  // },
+  methods: {
+    removeTodo(todoItem, index) {
+      this.$emit('removeTodo', todoItem, index);
+    },
+    toggleComplete(todoItem, index) {
+      this.$emit('toggleItem', todoItem,index);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 50px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .checkBtnCompleted {
+    color: #b3adad;
+  }
+  .textCompleted {
+    color: #b3adad;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+</style>

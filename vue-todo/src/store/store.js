686a0d7333952fdc1e4d1c94ffce9a3609bac640
DDOE49 <yhwy1965@naver.com>
2020-12-23 11:42:57 +0900

    vue store modules 속성으로 관리

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,34 +1,39 @@
 import Vue from 'vue'
 import Vuex from  'vuex'
-import * as getters from './getters'
-import * as mutations from './mutations'
+// import * as getters from './getters'
+// import * as mutations from './mutations'
+import todoApp from './modules/todoApp'
 
 Vue.use(Vuex); // use: vue 플러그인 뷰를 사용하는 모든영역에 global을 적용하고 싶을 때
 
-const storage = {
-    fetch() {
-       const arr = [];
-        if (localStorage.length > 0)
-       {
-         for (let i = 0; i < localStorage.length; i++)
-         {
-           if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
-           {
-             // this.todoItems.push(localStorage.key(i));
-             arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-           }
-         }
-       }
-       return arr;
-    }
-};
+// const storage = {
+//     fetch() {
+//        const arr = [];
+//         if (localStorage.length > 0)
+//        {
+//          for (let i = 0; i < localStorage.length; i++)
+//          {
+//            if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
+//            {
+//              // this.todoItems.push(localStorage.key(i));
+//              arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+//            }
+//          }
+//        }
+//        return arr;
+//     }
+// };
 
 export const store = new Vuex.Store({
-    state: {
-        todoItems: storage.fetch()
-    },
-    getters,
-    mutations
+
+    modules: {
+      todoApp
+    }
+    // state: {
+    //     todoItems: storage.fetch()
+    // },
+    // getters,
+    // mutations
     // getters: {
     //   storedTodoItems(state) {
     //     return state.todoItems;
